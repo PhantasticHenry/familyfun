a53791ee04da363317187087331defc727218e96
github username <github email address>
2020-01-10 16:07:14 -0800

    changed filename to environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,7 @@
 require_relative "./familyfun/version"
 require_relative './familyfun/cli'
+require_relative './familyfun/scraper'
+require_relative './familyfun/event'
 
 require "tty-prompt"
 require 'pry'
