872d1c95c8e4db8ee1733d8488a9d0ac8feb85a4
github username <github email address>
2020-01-11 13:03:55 -0800

    scraped more info from differnt URL

--- lib/familyfun/scraper.rb
+++ lib/familyfun/scraper.rb
@@ -1,6 +1,5 @@
 class Familyfun::Scraper
 
-    @@user_input = 1
     def self.scrape_events
         doc = Nokogiri::HTML(open("https://www.parentmap.com/calendar"))
         events = doc.search("div.col-content")
@@ -15,8 +14,22 @@ class Familyfun::Scraper
     end
 
     def self.scrape_details
+        event_details = []
         website = Nokogiri::HTML(open("https://www.parentmap.com/calendar/oly-ice-seasonal-skating"))
-        event_details = website.css("div.body p").text
+        event_details << info = website.css("div.body p").text
+        more_info = website.css("ul.info-list")
+        more_info.each do |i|
+            event_details << price = "Cost: " + i.css("div.field_event_cost").text
+            event_details << rec_ages = i.css("div.field_age_recommendation").text
+            event_details << address1 = "Address: " + i.css("p.address").text.split.join(" ")
+        end
+        event_details
+        # binding.pry
     end
+
+    # def self.scrape_free_events
+    #     doc = Nokogiri::HTML(open("https://www.parentmap.com/calendar?geolocation-lat=&geolocation-lng=&geolocation_geocoder_google_geocoding_api_state=1&date=now&keys=&geolocation_geocoder_google_geocoding_api=&geolocation=5&field_event_types_value%5Bfree%5D=free"))
+    #     free_events = doc.search("")
+    # end
 
 end
