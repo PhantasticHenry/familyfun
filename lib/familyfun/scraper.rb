c08672bad3bb084d49e42969262f30e49bf8d4a5
github username <github email address>
2020-01-11 21:43:32 -0800

    cleaned up scraper method

--- lib/familyfun/scraper.rb
+++ lib/familyfun/scraper.rb
@@ -9,13 +9,13 @@ class Familyfun::Scraper
             event.date = e.search("h4.event-date").text.strip.gsub(/[[:space:]]/, ' ').split(" ").join(" ")
             event.location = e.search("h5.event-location").text.gsub(/[[:space:]]/, ' ').split(" ").join(" ")
             event.url = "https://www.parentmap.com" + (e.search("a").attr("href").text)
-            event.details = []
+            # event.details = []
                 
-                website = Nokogiri::HTML(open("https://www.parentmap.com" + (e.search("a").attr("href").text)))
-                event.details << website.search("div.body p").text
-                event.details << ("Cost: " + website.css("div.field_event_cost").text) unless (website.css("div.field_event_cost").text == "")
-                event.details << website.css("div.field_age_recommendation").text
-                event.details << "Address: " + website.css("p.address").text.split.join(" ")
+                more_info = Nokogiri::HTML(open("https://www.parentmap.com" + (e.search("a").attr("href").text)))
+                event.details = more_info.search("div.body p").text + "\n\n" + more_info.css("div.field_age_recommendation").text
+                event.price = (more_info.css("div.field_event_cost").text) unless (more_info.css("div.field_event_cost").text == "")
+                # event.details << more_info.css("div.field_age_recommendation").text
+                event.address = more_info.css("p.address").text.split.join(" ")
         end
     end
 end
