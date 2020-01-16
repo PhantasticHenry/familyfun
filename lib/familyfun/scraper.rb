01fb7c6cc53146b15660df29ebe44d8e5edb518d
github username <github email address>
2020-01-15 16:17:14 -0800

    erase unnecessary code

--- lib/familyfun/scraper.rb
+++ lib/familyfun/scraper.rb
@@ -9,12 +9,10 @@ class Familyfun::Scraper
             event.date = e.search("h4.event-date").text.strip.gsub(/[[:space:]]/, ' ').split(" ").join(" ")
             event.location = e.search("h5.event-location").text.gsub(/[[:space:]]/, ' ').split(" ").join(" ")
             event.url = "https://www.parentmap.com" + (e.search("a").attr("href").text)
-            # event.details = []
-                
+
                 more_info = Nokogiri::HTML(open("https://www.parentmap.com" + (e.search("a").attr("href").text)))
                 event.details = more_info.search("div.body p").text + "\n\n" + more_info.css("div.field_age_recommendation").text
                 event.price = (more_info.css("div.field_event_cost").text) unless (more_info.css("div.field_event_cost").text == "")
-                # event.details << more_info.css("div.field_age_recommendation").text
                 event.address = more_info.css("p.address").text.split.join(" ")
         end
     end
